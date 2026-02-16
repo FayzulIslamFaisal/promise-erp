40fea537ac01353b73aca3b57d4a828049bb5fa7
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2026-02-16 17:51:37 +0600

    blog details start

--- app/(root)/blog/[slug]/page.tsx
+++ app/(root)/blog/[slug]/page.tsx
@@ -1,10 +1,22 @@
+import BlogDetailWrapper from "@/components/root/blog/BlogDetailWrapper";
+import { Suspense } from "react";
 
-const BlogDetailsPage = () => {
-  return (
-    <div>
-      <h1>Blog Details Page</h1>
-    </div>
-  )
+interface BlogDetailsPageProps {
+  params: Promise<{ slug: string }>;
 }
 
-export default BlogDetailsPage
+const BlogDetailsPage = async ({ params }: BlogDetailsPageProps) => {
+  const { slug } = await params;
+
+  return (
+    <Suspense fallback={<div className="min-h-screen bg-background">
+      <div className="container mx-auto px-4 py-16 space-y-16">
+        <h1 className="text-2xl text-center">Loading...</h1>
+      </div>
+    </div>}>
+      <BlogDetailWrapper slug={slug} />
+    </Suspense>
+  );
+};
+
+export default BlogDetailsPage;
